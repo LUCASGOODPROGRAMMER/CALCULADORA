691a6922dec0aeaed2adaee2b2f6de7606514186
Lucas Hubner <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2025-06-28 16:57:44 -0400

    arrumando alguns bugs

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ capturaBtn.addEventListener("click", (e) => {
     }
     return;
   }
-  document.querySelector("#aviso-erro").innerHTML ""
+  document.querySelector("#aviso-erro").innerHTML = ''
   for (let i = 0; i <= multiplicador; i++) {
     const p = document.createElement("p");
     p.textContent = `${number} x ${i} = ${number * i}`;
